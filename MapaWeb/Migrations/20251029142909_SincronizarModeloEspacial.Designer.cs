53d1f84fac36758023a980f677fe1ffbec698863
sdiprinzio <segudp@gmail.com>
2025-10-30 10:50:13 -0300

    rutas Bs As a Córdoba

--- MapaWeb/Migrations/20251029142909_SincronizarModeloEspacial.Designer.cs
+++ MapaWeb/Migrations/20251029142909_SincronizarModeloEspacial.Designer.cs
@@ -3,6 +3,7 @@ using MapaWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 
@@ -11,9 +12,11 @@ using NetTopologySuite.Geometries;
 namespace MapaWeb.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251029142909_SincronizarModeloEspacial")]
+    partial class SincronizarModeloEspacial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MapaWeb/Migrations/20251029142909_SincronizarModeloEspacial.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NetTopologySuite.Geometries;
+
+#nullable disable
+
+namespace MapaWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class SincronizarModeloEspacial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+           
+        }
+    }
+}
